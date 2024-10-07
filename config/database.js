0fb32461929caddbb93f4278fd509c233c90c58c
Hoàng Phan <nvhoangphan1801@gmail.com>
2024-10-07 15:42:17 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL, {
+            serverSelectionTimeoutMS: 30000, // Tăng thời gian chờ lên 30 giây
+            socketTimeoutMS: 45000,
+        })
         console.log("Connect succes!")
     } catch (error) {
         console.log("Connect error!")
