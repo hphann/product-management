b4ede8887d4561bb5bdd0373352aee33fa58f815
Hoàng Phan <nvhoangphan1801@gmail.com>
2024-10-10 23:06:53 +0700

    Upload image TinyMCE

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const path = require('path')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser') 
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const flash = require('express-flash')
-const multer = require('multer')
 require('dotenv').config()
 
 const database = require("./config/database")
@@ -33,6 +33,12 @@ app.use(session({
     cookie: { maxAge: 60000 }
 }));
 app.use(flash())
+
+//TinyMCE
+app.use(
+    '/tinymce',
+    express.static(path.join(__dirname, 'node_modules', 'tinymce'))
+)
 
 //App Locals Var
 app.locals.prefixAdmin = systemConfig.prefixAdmin
